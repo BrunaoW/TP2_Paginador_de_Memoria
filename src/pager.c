39dd28b74465213020637cc9fa2c0c7d6b5deb67
Bruno Wilson <brunowilson4@gmail.com>
2023-05-29 17:14:04 -0300

    Muda permissão do arquivo .sh, adiciona gitignore e cria pager.c

---
+++ src/pager.c
@@ -0,0 +1,26 @@
+#include "pager.h"
+
+
+void pager_init(int nframes, int nblocks) {
+
+}
+
+void pager_create(pid_t pid) {
+
+}
+
+void *pager_extend(pid_t pid) {
+
+}
+
+void pager_fault(pid_t pid, void *addr) {
+
+}
+
+int pager_syslog(pid_t pid, void *addr, size_t len) {
+    return 0;
+}
+
+void pager_destroy(pid_t pid) {
+
+}
