cc84ae5e07e919adffc035aec0396d3f2346993d
Gustavo Augusto <gustavo.augusto.pires@hotmail.com>
2023-05-29 23:44:59 -0300

    Criando lista de PID, método create e método destroy

--- src/pager.c
+++ src/pager.c
@@ -1,26 +1,82 @@
 #include "pager.h"
 
+#define MAX_SIZE 100
 
-void pager_init(int nframes, int nblocks) {
+typedef struct
+{
+    pid_t data[MAX_SIZE];
+    int size;
+} PidList;
 
+void addPid(PidList *list, pid_t pid)
+{
+    if (list->size < MAX_SIZE)
+    {
+        list->data[list->size] = pid;
+        list->size++;
+    }
+    else
+    {
+        printf("Full of elements.\n");
+    }
 }
 
-void pager_create(pid_t pid) {
+void removePid(PidList *list, pid_t pid)
+{
+    int index = -1;
+    for (int i = 0; i < list->size; i++)
+    {
+        if (list->data[i] == pid)
+        {
+            index = i;
+            break;
+        }
+    }
 
+    if (index != -1)
+    {
+        for (int i = index; i < list->size - 1; i++)
+        {
+            list->data[i] = list->data[i + 1];
+        }
+        list->size--;
+    }
+    else
+    {
+        printf("PID not found in the list.\n");
+    }
 }
 
-void *pager_extend(pid_t pid) {
+PidList pidList;
+int _nframes;
+int _nblocks;
 
+void pager_init(int nframes, int nblocks)
+{
+    pidList.size = 0;
+    _nframes = nframes;
+    _nblocks = nblocks;
 }
 
-void pager_fault(pid_t pid, void *addr) {
-
+void pager_create(pid_t pid)
+{
+    addPid(&pidList, pid);
 }
 
-int pager_syslog(pid_t pid, void *addr, size_t len) {
+void *pager_extend(pid_t pid)
+{
+}
+
+void pager_fault(pid_t pid, void *addr)
+{
+}
+
+int pager_syslog(pid_t pid, void *addr, size_t len)
+{
     return 0;
 }
 
-void pager_destroy(pid_t pid) {
-
+void pager_destroy(pid_t pid)
+{
+    removePid(&pidList, pid);
 }
