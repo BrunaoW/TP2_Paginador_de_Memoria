1aa4dd0c8330dd0a7ae2b029016172b7fc877463
Bruno Wilson <brunowilson4@gmail.com>
2023-06-05 23:08:50 -0300

    implementa pager_init

--- src/pager.c
+++ src/pager.c
@@ -2,88 +2,72 @@
 #include "mmu.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <pthread.h>
 
-#define MAX_SIZE 100
+typedef unsigned bit;
 
-typedef struct
-{
-    pid_t data[MAX_SIZE];
-    int size;
-} PidList;
+typedef struct frame {
+    pid_t pid;
+    int number;
+    bit reference;
+} frame_t;
 
-void addPid(PidList *list, pid_t pid)
-{
-    if (list->size < MAX_SIZE)
-    {
-        list->data[list->size] = pid;
-        list->size++;
+typedef struct frame_table {
+    int num_frames;
+    int* frames_list;
+    int* blocks_list;
+} frame_table_t;
+
+typedef struct frame_table_list {
+    pid_t pid;
+    frame_table_t *table_list;
+} frame_table_list_t;
+
+int* disk_blocks;
+int blocks_in_use;
+
+frame_t* frames_list;
+frame_table_list_t* frame_table_list;
+
+int current_page_clock = 0;
+
+void pager_init(int nframes, int nblocks) {
+
+    // Inicializar quadros de memoria e blocos no disco
+    disk_blocks = (int*) malloc(nblocks * sizeof(int));
+    frames_list = (frame_t*) malloc(nframes * sizeof(frame_t));
+    frame_table_list = (frame_table_list_t*) malloc(sizeof(frame_table_list_t));
+
+    blocks_in_use = 0;
+
+    for (int i = 0; i < nblocks; i++) {
+        disk_blocks[i] = 0;
     }
-    else
-    {
-        printf("Full of elements.\n");
+
+    for (int i = 0; i < nframes; i++) {
+        frames_list[i].pid = -1;
+        frames_list[i].number = 0;
+        frames_list[i].reference = 0;
     }
 }
 
-void removePid(PidList *list, pid_t pid)
-{
-    int index = -1;
-    for (int i = 0; i < list->size; i++)
-    {
-        if (list->data[i] == pid)
-        {
-            index = i;
-            break;
-        }
-    }
-
-    if (index != -1)
-    {
-        for (int i = index; i < list->size - 1; i++)
-        {
-            list->data[i] = list->data[i + 1];
-        }
-        list->size--;
-    }
-    else
-    {
-        printf("PID not found in the list.\n");
-    }
+void pager_create(pid_t pid) {
+    
 }
 
-PidList pidList;
-int _nframes;
-int _nblocks;
-
-void pager_init(int nframes, int nblocks)
-{
-    pidList.size = 0;
-    _nframes = nframes;
-    _nblocks = nblocks;
-}
-
-void pager_create(pid_t pid)
-{
-    addPid(&pidList, pid);
-}
-
-void *pager_extend(pid_t pid)
-{
+void *pager_extend(pid_t pid) {
     int *__attribute__((aligned(4096))) ptr;
     unsigned long desiredAddress = 0x60000000;
     ptr = (int *)desiredAddress;
     return ptr;
 }
 
-void pager_fault(pid_t pid, void *addr)
-{
+void pager_fault(pid_t pid, void *addr) {
 }
 
-int pager_syslog(pid_t pid, void *addr, size_t len)
-{
+int pager_syslog(pid_t pid, void *addr, size_t len) {
     return 0;
 }
 
-void pager_destroy(pid_t pid)
-{
-    removePid(&pidList, pid);
+void pager_destroy(pid_t pid) {
 }
